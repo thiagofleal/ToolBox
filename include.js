944a4d9bdf69b1f010dc1f6b0ed77d3fbfe83eda
thiagofleal <thiagofernandesleal@yahoo.com.br>
2019-07-17 17:30:31 -0300

    Toolbox initial version

---
+++ include.js
@@ -0,0 +1,54 @@
+var script = document.currentScript;
+
+function scriptFileName() {
+	return script.src;
+}
+
+function frameworkPath(){
+	return scriptFileName().replace("include.js", "");
+}
+
+document.writeln(
+	'<link '
+	+ 'type="text/css" '
+	+ 'rel="stylesheet" '
+	+ 'href="'
+	+ frameworkPath() + "css/normalize.css"
+	+ '">'
+);
+
+document.writeln(
+	'<script '
+	+ 'src="'
+	+ frameworkPath() + "js/jquery.min.js"
+	+ '"></script>'
+);
+
+document.writeln(
+	'<link '
+	+ 'type="text/css" '
+	+ 'rel="stylesheet" '
+	+ 'href="'
+	+ frameworkPath() + "css/bootstrap.min.css"
+	+ '">'
+);
+
+document.writeln(
+	'<!--[if lt IE 9]>'
+);
+document.writeln(
+	'<script src="' + frameworkPath() + "js/html5shiv.js" + '"></script>'
+);
+document.writeln(
+	'<script src="' + frameworkPath() + "js/respond.js" + '"></script>'
+);
+document.writeln(
+	'<![endif]-->'
+);
+
+window.onload = function(){
+	var script = document.createElement('script');
+	script.type = "text/javascript";
+	script.src = frameworkPath() + "js/bootstrap.min.js";
+	document.body.appendChild(script);
+};
